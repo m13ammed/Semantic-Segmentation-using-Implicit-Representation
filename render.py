4d85d580241a485ddfb342ceb2da4247f63fd881
Karim ElGhandour <kghandour@live.com>
2022-11-19 14:34:24 +0100

    Scene construct and cleanup

--- render.py
+++ render.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import open3d as o3d
 import torch
+from read_scene import ReadScene
 
 print_separator = "==========="
 SH_C0 = 0.28209479177387814
@@ -38,98 +39,6 @@ SH_C4 = [
 def normalize(arr):
     return (arr-arr.min())/(arr.max()-arr.min())
 
-def debug_ckpt(ckpt):
-    print(print_separator)
-    print("Printing CKPT")
-    for key in ckpt.keys():
-        print(key)
-        print(ckpt[key])
-    print(print_separator)
-    print("Printing State_Dict")
-    for key in ckpt['state_dict'].keys():
-        print(key)
-        print(ckpt['state_dict'][key])
-    print(print_separator)
-    print("Printing SH")
-    print("Length: ", len(ckpt['state_dict']['model.sh_data']))
-    print(ckpt['state_dict']['model.sh_data'][0])
-    print(print_separator)
-
-def inspect_init(init):
-    print(print_separator)
-    print("Printing init")
-    print("Length: ",len(init))
-    print(init)
-    print(print_separator)
-    
-def inspect_thick(thick):
-    print(print_separator)
-    print("Printing -thick-")
-    print("Length: ", len(thick))
-    print(thick)
-    print(print_separator)
-
-def inspect_trans(trans_info):
-    print(print_separator)
-    print("Printing trans info")
-    for file in trans_info.files:
-        print(file)
-        print(trans_info[file])
-    print(print_separator)
-
-def read_dataset(scene_folder) -> dict:
-    """
-    Reads the dataset from the folder
-    :return: dict {'ckpt','init','thick','trans_info'}
-    """
-    ## ckpt
-    ckpt_path = os.path.join(scene_folder, "last.ckpt")
-    if not os.path.exists(ckpt_path):
-        ckpt_path = os.path.join(scene_folder, "data.ckpt")
-    ckpt = torch.load(ckpt_path, map_location=torch.device('cpu'))
-
-    ## 
-    init_path = os.path.join(scene_folder, "init.npy")
-    init = np.load(init_path)
-
-    ##     
-    thick_path = os.path.join(scene_folder, "thick.npy")
-    thick = np.load(thick_path)
-
-    ##
-    trans_info_path = os.path.join(scene_folder,"trans_info.npz")
-    trans_info = np.load(trans_info_path)
-
-    return {"ckpt":ckpt, "init": init, "thick":thick, "trans_info":trans_info}
-   
-def parse_ckpt(ckpt) -> dict:
-    """
-    Reads and parses the ckpt 'state_dict'
-    :return: dict {'grid', 'density', 'sh', 'res'}
-    """
-    density = ckpt["state_dict"]["model.density_data"].detach().cpu()
-    links_idx = ckpt["state_dict"]["model.links_idx"].detach().cpu()
-    sh_data = ckpt['state_dict']['model.sh_data'].detach().cpu()
-    valid = torch.where(density > 0.0)[0].long()
-    density, links_idx = density[valid], links_idx[valid].long()
-    sh_data = sh_data[valid].numpy().astype(np.float64)
-
-    resolution = (
-        ckpt["reso_list"]["reso_idx"] 
-        if "reso_list" in ckpt.keys() else 
-        [256, 256, 256]
-    )
-
-    links_idx = torch.stack(
-        [
-            links_idx // (resolution[1] * resolution[2]),
-            links_idx % (resolution[1] * resolution[2]) // resolution[2],
-            -links_idx % resolution[2],
-        ],
-        -1,
-    )
-
-    return {"grid":links_idx, "density":density, "sh":sh_data, "res":resolution}
 
 def vis_vox(links_idx, density):
     """
@@ -377,7 +286,9 @@ def render_img(processed_ckpt, cam_params, img_params, batch_size: int = 1):
     return all_rgb_out.view(img_params['height'], img_params['width'], -1)
 
 if __name__ == "__main__":
-    scene_folder = "/Users/kghandour/development/PeRFception-ScanNet/plenoxel_scannet_scene0000_00/"
+    perfception_scannet_folder = "../PeRFception-ScanNet"
+    original_scannet_folder = "../ScanNet/scans"
+    scene_name = "plenoxel_scannet_scene0000_00"
     render_dir = "./images"
     cam_params = {
         "fx":1170.187988,
@@ -389,14 +300,20 @@ if __name__ == "__main__":
         "width":1296,
         "height":968
     } ## Probabbly high res. Low res might be 640, 480
+    scene = ReadScene(
+        perf_folder= perfception_scannet_folder,
+        orig_scannet_folder= original_scannet_folder,
+        scene_name=scene_name
+    )
+
     os.makedirs(render_dir, exist_ok=True)
-    data = read_dataset(scene_folder= scene_folder)
-    processed_ckpt = parse_ckpt(ckpt=data['ckpt'])
-    im = render_img(processed_ckpt=processed_ckpt, cam_params=cam_params, img_params=img_params)
+    # data = read_dataset(scene_folder= scene_folder)
+    # processed_ckpt = parse_ckpt(ckpt=data['ckpt'])
+    # im = render_img(processed_ckpt=processed_ckpt, cam_params=cam_params, img_params=img_params)
 
-    from matplotlib import pyplot as plt
-    plt.imshow(data, interpolation='nearest')
-    plt.show()
+    # from matplotlib import pyplot as plt
+    # plt.imshow(data, interpolation='nearest')
+    # plt.show()
 
-    vis_vox(links_idx= processed_ckpt['grid'], density=processed_ckpt['density'])
+    vis_vox(links_idx= scene.links_idx, density=scene.density)
 
