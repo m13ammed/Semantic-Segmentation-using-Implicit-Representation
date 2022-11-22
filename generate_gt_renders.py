52594467d4f337b5ca32f184104e699576dec5f8
Karim ElGhandour <kghandour@live.com>
2022-11-22 17:04:13 +0100

    Meets GT but flipped vertically

--- generate_gt_renders.py
+++ generate_gt_renders.py
@@ -1,34 +1,89 @@
 from pytorch3d.io import IO
 import torch
+import numpy as np
+from locations_const import *
 from pytorch3d.renderer import (
     BlendParams,
     FoVPerspectiveCameras,
+    PerspectiveCameras,
     MeshRasterizer,
     MeshRenderer,
     PointLights,
     RasterizationSettings,
     SoftPhongShader,
 )
+
+from read_scene import Scene
 def generate_gt_renders(
-    poses, num_views: int = 1, polygon_path: str = "seg.ply", device = torch.device("cpu")
+    poses, num_views: int = 1, polygon_path: str = "seg.ply", device = torch.device("cpu"), intrinsic = None
 ):
     mesh = IO().load_mesh(polygon_path, device=device)
+    IO().save_mesh(mesh, "out.ply")
+    
     verts = mesh.verts_packed()
     N = verts.shape[0]
     center = verts.mean(0)
-    mesh.offset_verts_(-(center.expand(N, 3)))
+    # mesh.offset_verts_(-(center.expand(N, 3)))
     lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
 
+    M = torch.eye(3).to(device=device)
+    ## Flip Y and Z
+    # M[0,0]=1
+    # M[1,1]=0
+    # M[1,2]=1
+    # M[2,2]=0
+    # M[2,1]=1
 
-    R = poses[0][:3,:3]
-    T = poses[0][:3,3:]
-    print(R,T)
+    ### Flip X and Y
+    # M[0,0]=0
+    # M[0,1]=1
+    # M[1,1]=0
+    # M[1,0]=1
+    # M[2,2]=1
+    # M[2,1]=0
 
-    cameras = FoVPerspectiveCameras(device=device, R=R, T=T)
-    print(cameras)
+    pose = poses[0]
+    pose = torch.tensor(pose)
+    pose[0,1]=-pose[0,1]
+    pose = pose.inverse()
+    # pose = torch.linalg.inv(pose).contiguous()
+
+
+    # M[0,0]=-1.0
+    # M[1,1]=-1.0
+    # M[2,2]=-1.0
+
+    R = pose[:3,:3].T
+    # R[:,2] *= -1
+    # R = R.T
+    # print(R)
+    # R[2,] *= -1
+    # R = R.T
+    print(R)
+    # print(R)
+    T = pose[:3,3:]
+    # print(T)
+    # print(T, T.shape)
+    # R = M @ R @ M.inverse()
+    R= np.array(R)
+    # print(R)
+    T = M @ T
+    T = np.array(T)
+ 
+    # cameras = FoVPerspectiveCameras(device=device,R=np.array([R]), T=T.T)
+    cameras = PerspectiveCameras(
+        focal_length=((cam_params['fy'], cam_params['fx']),),
+        principal_point=((cam_params['my'], cam_params['mx']),),
+        in_ndc=False,
+        image_size=((img_params['height'], img_params['width']),),
+        device=device,
+        # K = [intrinsic],
+        R=np.array([R]),
+        T=T.T
+    )
     raster_settings = RasterizationSettings(
-        image_size=[480, 640], blur_radius=0.0, faces_per_pixel=1
-        # image_size=128, blur_radius=0.0, faces_per_pixel=1
+        # image_size=[480, 640], blur_radius=0.0, faces_per_pixel=1
+        image_size=128, blur_radius=0.0, faces_per_pixel=1
     ) 
     blend_params = BlendParams(sigma=1e-4, gamma=1e-4, background_color=(0.0, 0.0, 0.0))
     renderer = MeshRenderer(
@@ -40,7 +95,7 @@ def generate_gt_renders(
     meshes = mesh.extend(num_views)
 
     # Render the cow mesh from each viewing angle
-    target_images = renderer(meshes, cameras=cameras, lights=lights)
+    target_images = renderer(meshes, cameras=cameras[0], lights=lights)
     return cameras, target_images[..., :3]
 
 
--- render_gt.py
+++ render_gt.py
@@ -6,8 +6,10 @@ from PIL import Image
 import numpy as np
 import glob
 
+from read_scene import Scene
 
-def export_images(target_images):
+
+def export_images(target_images, show_only=False):
     clamp_and_detach = lambda x: x.clamp(0.0, 1.0).cpu().detach().numpy()
     for idx in range(target_images.shape[0]):
         image = (clamp_and_detach(target_images[idx, ..., :3]))
@@ -17,7 +19,10 @@ def export_images(target_images):
             assert tensor.shape[0] == 1
             tensor = tensor[0]
         img = Image.fromarray(tensor)
-        img.save(os.path.join("./out/",str(idx)+".png"))
+        if(show_only):
+            img.show()
+        else:
+            img.save(os.path.join("./out/",str(idx)+".png"))
 
 def find_files(dir, exts):
     if os.path.isdir(dir):
@@ -37,13 +42,19 @@ if __name__ == "__main__":
     pose_files = find_files(pose_folder, exts=["*.txt"])
     assert len(pose_files) > 0, f"{pose_folder} does not contain poses"
     frame_ids = sorted([os.path.basename(f).rstrip(".txt") for f in pose_files])
+    # scene = Scene(perf_folder=perfception_scannet_folder, orig_scannet_folder= original_scannet_folder, scene_name=plenoxel_prefix+scene_name)
 
     print(f"loading poses - {len(frame_ids)}")
     poses = np.stack(
         [np.loadtxt(os.path.join(pose_folder, f"{f}.txt")) for f in frame_ids],
         axis=0,
     )
+    trans_info = np.load(os.path.join(perfception_scannet_folder,plenoxel_prefix+scene_name, "trans_info.npz"))
+
     poses = poses.astype(np.float32)
+
+    # poses[:, :3, 3] *= scene_scale
+    ## zero mean [end]
     # numerics = np.all(
     #     (~np.isinf(poses) * ~np.isnan(poses) * ~np.isneginf(poses)).reshape(-1, 16),
     #     axis=1,
@@ -58,12 +69,11 @@ if __name__ == "__main__":
     intrinsic[[2, 3], [2, 3]] = 1
 
     # load trans_info
-    trans_info = np.load(os.path.join(perfception_scannet_folder,plenoxel_prefix+scene_name, "trans_info.npz"))
 
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
         torch.cuda.set_device(device)
     else: device = torch.device("cpu")
     
-    cameras, target_images = generate_gt_renders(poses=poses, polygon_path=polygon_file, device=device)
-    export_images(target_images=target_images)
+    cameras, target_images = generate_gt_renders(poses=poses, polygon_path=polygon_file, device=device, intrinsic=intrinsic)
+    export_images(target_images=target_images, show_only = True)
